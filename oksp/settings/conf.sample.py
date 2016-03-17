9242ba0b0542539c7f1e0e261e868096390a9cf5
Siddharth Bulia <siddharthbulia@gmail.com>
2016-03-17 19:12:44 +0530

    Initialising repo

---
+++ oksp/settings/conf.sample.py
@@ -0,0 +1,21 @@
+"""
+Configuration file for settings
+"""
+
+SECRET_KEY = '<SECRET KEY>'
+
+ALLOWED_HOSTS = ['*']
+
+DB_NAME = 'oksp'
+
+DB_USERNAME = 'oksp'
+
+DB_PASSWORD = 'oksp'
+
+DB_HOST_NAME = 'localhost'
+
+DB_PORT = '5432'
+
+ADMINS_EMAIL_LIST = [
+    # ('Name', 'email@example.com'),
+]
