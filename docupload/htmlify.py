3788b2f938dedd052a9eb36c4864ac797ccfb417
Meet Udeshi <mudeshi1209@gmail.com>
2016-05-22 20:33:01 +0530

    Change to using /tmp dir for media and extract to media/docs/<doc-name>/

--- docupload/htmlify.py
+++ docupload/htmlify.py
@@ -12,30 +12,33 @@ def get_html(doc_file):
     '''Uses pypandoc to convert uploaded file to HTML5'''
 
     tmp_loc = '/tmp/uploaded_' + str(doc_file)
-    
+
     with open(tmp_loc, 'wb') as tmp_file:
         for chunk in doc_file.chunks():
             tmp_file.write(chunk)
-    html = pypandoc.convert( 
+    html = pypandoc.convert(
         tmp_loc,
         'html5',
-        extra_args=['--extract-media=']
+        extra_args=['--extract-media=/tmp']
         )
-    img_counter = 1
+
+    os.remove(tmp_loc)
+    return html
+
+def shift_media(html, doc_name, media_dir):
     soup = BeautifulSoup(html, 'lxml')
     for img in soup.findAll('img'):
-        img_name = '/media/' + str(doc_file) + '_' +str(img_counter) + '.jpg'
-        img['src'] = img_name
-        img_counter += 1
-    img_counter -= 1
-    for img_original in glob.glob('media/image*'):
-        img_name = 'media/' + str(doc_file) + '_' +str(img_counter) + '.jpg'
-        os.rename(img_original, img_name)
-        img_counter -= 1
+        img_src = img['src']
+        if img_src[:4] != 'http' and img_src[:2] != '//':
+            img_src = img_src.replace(media_dir, '/media/docs/' + doc_name)
+        img['src'] = img_src
+
+    os.mkdir('media/docs/' + doc_name)
+    for image_original in glob.glob(media_dir + '/image*'):
+        image_final = 'media/docs/' + doc_name + '/' + image_original.split('/')[-1]
+        os.rename(image_original, image_final)
+
     html = str(soup)
-    os.remove(tmp_loc)
-
-
     return html
 
 
@@ -52,11 +55,12 @@ class HTMLifier():
         '''Middleware function to interface with different <format>_convert functions'''
 
         file_name = str(doc_file)
-        ext = file_name.split('.')[-1] 
+        ext = file_name.split('.')[-1]
         file_name = file_name[:len(file_name) - len(ext) - 1]
         doc_dir = self.doc_base_path
 
         html = get_html(doc_file)
+        html = shift_media(html, file_name, '/tmp/media')
 
         with open(doc_dir + file_name + '.html', 'wb') as doc_stored:
             doc_stored.write(bytes(html, 'utf-8'))
